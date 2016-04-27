e9803db1de7db65e36197803118993a679e6e420
focode <programarunesh@gmail.com>
2016-04-27 13:06:44 +0530

    second commit woth spring boot micro services

--- src/main/java/com/gotprint/controller/GotPrintController.java
+++ src/main/java/com/gotprint/controller/GotPrintController.java
@@ -1,16 +1,17 @@
 package com.gotprint.controller;
 
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,7 +56,7 @@ public class GotPrintController {
        	return notes;
     }
     
-    @RequestMapping(value="/delete/{id}/", method=RequestMethod.DELETE)
+    @RequestMapping(value="/delete/{id}/", method=RequestMethod.POST)
     @Transactional
     public String delete(@PathVariable Integer id){
     	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -70,10 +71,23 @@ public class GotPrintController {
 		}
     	return "failure";
     	
-    	
-    	
     }
-
-	
+    
+    @RequestMapping(value="/update", method=RequestMethod.POST,headers = "Content-type: application/*")
+    @Transactional
+    public ResponseEntity<?> update(@RequestBody Notes note){
+    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    	System.out.println("auth.getName::"+auth.getName());
+    	User users = userRepository.findByEmailId(auth.getName());
+    	List<Notes> notes = (List<Notes>) users.getNotes();
+    	for (Notes notes2 : notes) {
+    		if(note.getId() == notes2.getId()){
+    			// note belongs to user update it
+    			notesRepository.save(note);
+    			return new ResponseEntity<>(note, HttpStatus.OK);
+    		}
+    	}
+    	return new ResponseEntity<>("", HttpStatus.NO_CONTENT); 
+    }
     
 }
